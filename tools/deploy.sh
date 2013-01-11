808ef26d2e36a3dd4f9007c884ea65433294afee
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2013-01-11 17:25:18 +0100

    Add "update" command

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -11,14 +11,16 @@ ROOT=$(pwd)
 PREFIX=$1
 LIBSUFFIX=$2
 
-if [ "$PREFIX" == "" ]; then
-    PREFIX=$ROOT/usr
-    printf "** Prefix not set, installing into $PREFIX\n"
-fi
-
 export LD_LIBRARY_PATH=$PREFIX/lib$LIBSUFFIX
 export PKG_CONFIG_PATH=$LD_LIBRARY_PATH/pkgconfig
 export GI_TYPELIB_PATH=$LD_LIBRARY_PATH/girepository-1.0
+
+function update() {
+    cd $ROOT/$1
+    git pull
+    make || exit 1
+    make install
+}
 
 function build_package() {
     pkg-config --cflags --libs libzmq
@@ -40,27 +42,41 @@ function build_package() {
     make install
 }
 
-if [ ! -f $ROOT/$ZEROMQ_TARBALL ]; then
-    printf "\n** Fetching ZeroMQ\n"
-    wget $ZEROMQ_URL || die "ERROR: Could not fetch ZeroMQ"
+function build() {
+    if [ ! -f $ROOT/$ZEROMQ_TARBALL ]; then
+        printf "\n** Fetching ZeroMQ\n"
+        wget $ZEROMQ_URL || die "ERROR: Could not fetch ZeroMQ"
+    fi
+
+    printf "\n** Building ZeroMQ\n"
+    tar xfz $ZEROMQ_TARBALL
+    cd $ROOT/$ZEROMQ_BASE
+    ./configure --prefix=$PREFIX --libdir=$LD_LIBRARY_PATH
+    make -j 4
+    make install
+
+    build_package "ufo-core"
+    build_package "oclfft"
+    build_package "ufo-filters"
+
+    # Link the typelib because the girepository framework is not searching
+    # /usr/local on openSUSE systems.
+    if [[ $EUID -eq 0 ]]; then
+        TYPELIB_PATH=$(ls $GI_TYPELIB_PATH/Ufo-*.typelib)
+        TYPELIB=$(basename $TYPELIB_PATH)
+        ln -s $TYPELIB_PATH /usr/lib$LIBSUFFIX/girepository-1.0/$TYPELIB
+        ldconfig
+    fi
+}
+
+if [ "$PREFIX" == "" ]; then
+    PREFIX=$ROOT/usr
+    printf "** Prefix not set, installing into $PREFIX\n"
 fi
 
-printf "\n** Building ZeroMQ\n"
-tar xfz $ZEROMQ_TARBALL
-cd $ROOT/$ZEROMQ_BASE
-./configure --prefix=$PREFIX --libdir=$LD_LIBRARY_PATH
-make -j 4
-make install
-
-build_package "ufo-core"
-build_package "oclfft"
-build_package "ufo-filters"
-
-# Link the typelib because the girepository framework is not searching
-# /usr/local on openSUSE systems.
-if [[ $EUID -eq 0 ]]; then
-    TYPELIB_PATH=$(ls $GI_TYPELIB_PATH/Ufo-*.typelib)
-    TYPELIB=$(basename $TYPELIB_PATH)
-    ln -s $TYPELIB_PATH /usr/lib$LIBSUFFIX/girepository-1.0/$TYPELIB
-    ldconfig
+if [ "$PREFIX" == "update" ]; then
+    update "ufo-core"
+    update "ufo-filters"
+else
+    build
 fi
