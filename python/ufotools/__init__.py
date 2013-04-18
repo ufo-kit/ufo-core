87f39542093f4635458cbc4bfdd3cfea49c84c39
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2013-04-18 12:36:19 +0200

    Add some example documentation

--- python/ufotools/__init__.py
+++ python/ufotools/__init__.py
@@ -1,8 +1,10 @@
 from gi.repository import Ufo
 
+
 def _run(graph, config=None):
     sched = Ufo.Scheduler(config=config)
     sched.run(graph)
+
 
 class TaskWrapper(object):
     def __init__(self, task, graph, config=None):
@@ -18,19 +20,54 @@ class TaskWrapper(object):
         return self
 
     def run(self):
+        """Execute the implied task graph that this node is connected in."""
         _run(self.graph, self.config)
 
 
 class Factory(object):
+    """Wrapped task node factory."""
     def __init__(self, config=None):
         self.pm = Ufo.PluginManager(config=config)
         self.graph = Ufo.TaskGraph()
         self.config = config
 
     def get(self, name, **kwargs):
+        """Return a wrapped task instantiated from *name* and pre-populated
+        with property values in the *kwargs* dict.
+
+        Using
+
+            read = factory.get('reader', path='/some/where')
+
+        is equivalent to
+
+            read = pluginmanager.get_task('reader')
+            read.set_properties(path='/some/where')
+
+        Any *wrapped* task can be call chained, which in turn is an implicit
+        connection in the task graph. So, using
+
+            write(avg(read))
+            factory.run()
+
+        is equivalent to
+
+            g = Ufo.TaskGraph()
+            g.connect_nodes(read, avg)
+            g.connect_nodes(avg, write)
+
+            s = Ufo.Scheduler()
+            s.run(g)
+
+        Note, that wrapped tasks also have a run() method, so you can actually
+        start the computation with
+
+            write(avg(read)).run()
+        """
         task = self.pm.get_task(name)
         task.set_properties(**kwargs)
         return TaskWrapper(task, self.graph, self.config)
 
     def run(self):
+        """Exec
         _run(self.graph, self.config)
