21264078475baf22fd7fa1934e87cf15924adab4
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2013-12-12 16:54:20 +0100

    Fix #17: remove g_type_init for GLib >= 2.36.0

--- ufo/compat.h
+++ ufo/compat.h
@@ -29,6 +29,11 @@ gpointer    g_async_queue_timeout_pop (GAsyncQueue *queue,
                                        guint64 timeout);
 #endif
 
+/* Make g_type_init a no-op to prevent warnings in GLib versions >= 2.36.0 */
+#if GLIB_CHECK_VERSION(2, 36, 0)
+#define     g_type_init()
+#endif
+
 /* g_list_for() never existed, but it's nice to have anyway. */
 #define g_list_for(list, it) \
         for (it = g_list_first (list); \
