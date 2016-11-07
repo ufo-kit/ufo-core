bcd8e6407533740cb650dea2a519c0ad77abb297
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2016-11-07 11:45:41 +0100

    Document UFO_GPU_NODE_INFO_MAX_WORK_GROUP_SIZE

--- ufo/ufo-gpu-node.h
+++ ufo/ufo-gpu-node.h
@@ -67,6 +67,7 @@ struct _UfoGpuNodeClass {
  * @UFO_GPU_NODE_INFO_GLOBAL_MEM_SIZE: Global memory size
  * @UFO_GPU_NODE_INFO_MAX_MEM_ALLOC_SIZE: Maximum allocatable global memory size
  * @UFO_GPU_NODE_INFO_LOCAL_MEM_SIZE: Local memory size
+ * @UFO_GPU_NODE_INFO_MAX_WORK_GROUP_SIZE: Maximum work group size
  *
  * OpenCL device info types. Refer to the OpenCL standard for complete details
  * about each information.
