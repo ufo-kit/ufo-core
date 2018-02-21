ufo-kit/ufo-core

commit ec70e0631027d71be536b34e29d3d64a3f83d915

Remove daemon and messenger API
This is an effort to reduce the core API to absolutely necessary mechanisms.
With the zmq-pub and zmq-sub plugins, having networking code in the core is not
required anymore.

ufo/ufo-task-graph.c
@@ -22,10 +22,8 @@
 #include <json-glib/json-glib.h>
 #include <ufo/ufo-task-graph.h>
 #include <ufo/ufo-task-node.h>
-#include <ufo/ufo-remote-node.h>
 #include <ufo/ufo-input-task.h>
 #include <ufo/ufo-dummy-task.h>
-#include <ufo/ufo-remote-task.h>
 #include "compat.h"
 
 /**
@@ -45,7 +43,6 @@ G_DEFINE_TYPE (UfoTaskGraph, ufo_task_graph, UFO_TYPE_GRAPH)
 struct _UfoTaskGraphPrivate {
     UfoPluginManager *manager;
     GHashTable *json_nodes;
-    GList *remote_tasks;
     guint index;
     guint total;
 };
@@ -319,85 +316,6 @@ is_gpu_task (UfoTask *task, gpointer user_data)
     return ufo_task_uses_gpu (task);
 }
 
-static UfoTaskNode *
-build_remote_graph (UfoTaskGraph *remote_graph,
-                    GList *first,
-                    GList *last)
-{
-    UfoTaskNode *node = NULL;
-    UfoTaskNode *predecessor = NULL;
-
-    for (GList *it = g_list_next (first); it != last && it != NULL; it = g_list_next (it)) {
-        node = UFO_TASK_NODE (it->data);
-
-        if (predecessor != NULL)
-            ufo_task_graph_connect_nodes (remote_graph, predecessor, node);
-
-        predecessor = node;
-    }
-
-    g_assert (node != NULL);
-    return node;
-}
-
-static void
-create_remote_tasks (UfoTaskGraph *task_graph,
-                     UfoTaskGraph *remote_graph,
-                     UfoTaskNode *first,
-                     UfoTaskNode *last,
-                     UfoRemoteNode *remote)
-{
-    UfoTaskGraphPrivate *priv;
-    UfoTaskNode *task;
-    gchar *json;
-
-    json = ufo_task_graph_get_json_data (remote_graph, NULL);
-    priv = task_graph->priv;
-    ufo_remote_node_send_json (remote, UFO_REMOTE_MODE_STREAM, json);
-
-    task = UFO_TASK_NODE (ufo_remote_task_new ());
-    priv->remote_tasks = g_list_append (priv->remote_tasks, task);
-    ufo_task_node_set_proc_node (task, UFO_NODE (remote));
-
-    ufo_task_graph_connect_nodes (task_graph, first, task);
-    ufo_task_graph_connect_nodes (task_graph, task, last);
-
-    g_debug ("remote: connected %s -> [remote] -> %s",
-             ufo_task_node_get_identifier (first),
-             ufo_task_node_get_identifier (last));
-
-    g_free (json);
-}
-
-static void
-expand_remotes (UfoTaskGraph *task_graph,
-                GList *remotes,
-                GList *path)
-{
-    UfoTaskGraph *remote_graph;
-    UfoTaskNode *node;
-    GList *first;
-    GList *last;
-    GList *it;
-
-    first = g_list_first (path);
-    last = g_list_last (path);
-
-    remote_graph = UFO_TASK_GRAPH (ufo_task_graph_new ());
-    node = build_remote_graph (remote_graph, first, last);
-
-    if (ufo_graph_get_num_nodes (UFO_GRAPH (remote_graph)) == 0) {
-        ufo_task_graph_connect_nodes (remote_graph, UFO_TASK_NODE (ufo_dummy_task_new ()), node);
-    }
-
-    g_list_for (remotes, it) {
-        create_remote_tasks (task_graph, remote_graph,
-                             first->data, last->data, it->data);
-    }
-
-    g_object_unref (remote_graph);
-}
-
 static GList *
 nodes_with_common_ancestries (UfoTaskGraph *graph, GList *path)
 {
@@ -417,7 +335,6 @@ nodes_with_common_ancestries (UfoTaskGraph *graph, GList *path)
  * @graph: A #UfoTaskGraph
  * @resources: A #UfoResources objects
  * @n_gpus: Number of GPUs to expand the graph for
- * @expand_remote: %TRUE if remote nodes should be inserted
  *
  * Expands @graph in a way that most of the resources in @graph can be occupied.
  * In the simple pipeline case, the longest possible GPU paths are duplicated as
@@ -426,8 +343,7 @@ nodes_with_common_ancestries (UfoTaskGraph *graph, GList *path)
 void
 ufo_task_graph_expand (UfoTaskGraph *graph,
                        UfoResources *resources,
-                       guint n_gpus,
-                       gboolean expand_remote)
+                       guint n_gpus)
 {
     GList *path;
     GList *common;
@@ -486,21 +402,6 @@ ufo_task_graph_expand (UfoTaskGraph *graph,
         g_list_free (predecessors);
         g_list_free (successors);
 
-        if (expand_remote) {
-            GList *remotes;
-            guint n_remotes;
-
-            remotes = ufo_resources_get_remote_nodes (resources);
-            n_remotes = g_list_length (remotes);
-
-            if (n_remotes > 0) {
-                g_debug ("INFO Expand for %i remote nodes", n_remotes);
-                expand_remotes (graph, remotes, path);
-            }
-
-            g_list_free (remotes);
-        }
-
         g_debug ("INFO Expand for %i GPU nodes", n_gpus);
 
         for (guint i = 1; i < n_gpus; i++)
@@ -532,7 +433,6 @@ map_proc_node (UfoGraph *graph,
     UfoNode *proc_node;
     GList *successors;
     GList *it;
-    guint n_gpus;
 
     proc_node = UFO_NODE (g_list_nth_data (gpu_nodes, proc_index));
 
@@ -545,15 +445,10 @@ map_proc_node (UfoGraph *graph,
         ufo_task_node_set_proc_node (UFO_TASK_NODE (node), proc_node);
     }
 
-    n_gpus = g_list_length (gpu_nodes);
     successors = ufo_graph_get_successors (graph, node);
 
-    g_list_for (successors, it) {
+    g_list_for (successors, it)
         map_proc_node (graph, UFO_NODE (it->data), proc_index, gpu_nodes);
-
-        if (!UFO_IS_REMOTE_TASK (UFO_NODE (it->data)))
-            proc_index = n_gpus > 0 ? (proc_index + 1) % n_gpus : 0;
-    }
 
     g_list_free (successors);
 }
@@ -977,7 +872,6 @@ create_full_json_from_task_node (UfoTaskNode *task_node)
     node_object = json_object_new ();
     plugin_name = ufo_task_node_get_plugin_name (task_node);
 
-    /* plugin_name can be NULL for task graphs expanded with remote nodes */
     if (plugin_name == NULL)
         return NULL;
 
@@ -1060,10 +954,6 @@ ufo_task_graph_dispose (GObject *object)
         priv->manager = NULL;
     }
 
-    g_list_foreach (priv->remote_tasks, (GFunc) g_object_unref, NULL);
-    g_list_free (priv->remote_tasks);
-    priv->remote_tasks = NULL;
-
     nodes = g_hash_table_get_values (priv->json_nodes);
     g_list_foreach (nodes, (GFunc) g_object_unref, NULL);
     g_list_free (nodes);
@@ -1102,7 +992,6 @@ ufo_task_graph_init (UfoTaskGraph *self)
     self->priv = priv = UFO_TASK_GRAPH_GET_PRIVATE (self);
 
     priv->manager = NULL;
-    priv->remote_tasks = NULL;
     priv->json_nodes = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
     priv->index = 0;
     priv->total = 1;

CMakeLists.txt
@@ -55,11 +55,9 @@ endif ()
 #{{{ Dependencies
 set(PKG_GLIB2_MIN_REQUIRED "2.30")
 set(PKG_JSON_GLIB_MIN_REQUIRED "0.10.0")
-set(PKG_ZMQ_MIN_REQUIRED "2.1")
 
 find_package(OpenCL REQUIRED)
 find_package(PkgConfig REQUIRED)
-find_package(MPI)
 
 pkg_check_modules(GLIB2 glib-2.0>=${PKG_GLIB2_MIN_REQUIRED} REQUIRED)
 pkg_check_modules(GOBJECT2 gobject-2.0>=${PKG_GLIB2_MIN_REQUIRED} REQUIRED)
@@ -67,7 +65,6 @@ pkg_check_modules(GMODULE2 gmodule-2.0>=${PKG_GLIB2_MIN_REQUIRED} REQUIRED)
 pkg_check_modules(GTHREAD2 gthread-2.0>=${PKG_GLIB2_MIN_REQUIRED} REQUIRED)
 pkg_check_modules(GIO2 gio-2.0>=${PKG_GLIB2_MIN_REQUIRED} REQUIRED)
 pkg_check_modules(JSON_GLIB json-glib-1.0>=${PKG_JSON_GLIB_MIN_REQUIRED} REQUIRED)
-pkg_check_modules(ZMQ libzmq>=${PKG_ZMQ_MIN_REQUIRED})
 pkg_check_modules(PYTHON python)
 
 set(UFOCORE_DEPS
@@ -88,26 +85,6 @@ if (PYTHON_FOUND)
         list(APPEND UFOCORE_DEPS ${PYTHON_LIBRARIES})
     endif ()
 endif()
-
-if (MPI_C_FOUND)
-    option(WITH_MPI "Build with MPI support" OFF)
-
-    if (WITH_MPI)
-        set(CMAKE_C_COMPILER ${MPI_C_COMPILER})
-        include_directories(${MPI_C_INCLUDE_PATH})
-        list(APPEND UFOCORE_DEPS ${MPI_C_LIBRARIES})
-    endif ()
-endif ()
-
-if (ZMQ_FOUND)
-    option(WITH_ZMQ "Build with ZMQ support" ON)
-
-    if (WITH_ZMQ)
-        link_directories(${ZMQ_LIBRARY_DIRS})
-        include_directories(${ZMQ_INCLUDE_DIRS})
-        list(APPEND UFOCORE_DEPS ${ZMQ_LIBRARIES})
-    endif ()
-endif ()
 
 #{{{ Link dirs of dependencies
 link_directories(

bin/CMakeLists.txt
@@ -4,14 +4,12 @@ cmake_minimum_required(VERSION 2.6)
 add_executable(ufo-launch ufo-launch.c)
 add_executable(ufo-query ufo-query.c)
 add_executable(ufo-runjson ufo-runjson.c)
-add_executable(ufod ufod.c)
 
 target_link_libraries(ufo-launch ufo)
 target_link_libraries(ufo-query ufo)
 target_link_libraries(ufo-runjson ufo)
-target_link_libraries(ufod ufo)
 
-install(TARGETS ufod ufo-launch ufo-query ufo-runjson
+install(TARGETS ufo-launch ufo-query ufo-runjson
         RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
 
 install(PROGRAMS ${CMAKE_CURRENT_SOURCE_DIR}/ufo-prof
@@ -26,23 +24,6 @@ if (INSTALL_COMPLETION)
             DESTINATION ${BASH_COMPLETION_COMPLETIONSDIR}
             OPTIONAL)
 endif()
-#}}}
-#{{{ systemd .service file
-pkg_check_modules(SYSTEMD systemd)
-
-if (SYSTEMD_FOUND)
-    pkg_check_variable(systemd systemdsystemunitdir)
-
-    option(WITH_SYSTEMD "Install systemd unit file" OFF)
-
-    if (WITH_SYSTEMD)
-        configure_file(${CMAKE_CURRENT_SOURCE_DIR}/ufod.service.in
-                       ${CMAKE_CURRENT_BINARY_DIR}/ufod.service
-                       @ONLY IMMEDIATE)
-       install(FILES ${CMAKE_CURRENT_BINARY_DIR}/ufod.service
-               DESTINATION ${SYSTEMD_SYSTEMDSYSTEMUNITDIR})
-    endif ()
-endif ()
 #}}}
 #{{{ ufo-mkfilter
 set(CMAKE_INSTALL_TEMPLATEDIR "${CMAKE_INSTALL_FULL_DATADIR}/ufo/templates")

bin/meson.build
@@ -1,7 +1,8 @@
-progs = ['ufo-launch',
-         'ufo-query',
-         'ufo-runjson',
-         'ufod']
+progs = [
+    'ufo-launch',
+    'ufo-query',
+    'ufo-runjson',
+]
 
 foreach prog: progs
     sources = [

bin/ufod.c
@@ -1,143 +1,0 @@
-/*
- * Copyright (C) 2011-2013 Karlsruhe Institute of Technology
- *
- * This file is part of ufod.
- *
- * ufod is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * ufod is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with ufod.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include "config.h"
-
-#ifdef __APPLE__
-#include <OpenCL/cl.h>
-#else
-#include <CL/cl.h>
-#endif
-#include <signal.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ufo/ufo.h>
-
-
-static UfoDaemon *global_daemon = NULL;
-static gboolean done = FALSE;
-
-typedef struct {
-    gchar *addr;
-} Options;
-
-static Options *
-opts_parse (gint *argc, gchar ***argv)
-{
-    GOptionContext *context;
-    Options *opts;
-    gboolean show_version = FALSE;
-    GError *error = NULL;
-
-    opts = g_new0 (Options, 1);
-
-    GOptionEntry entries[] = {
-        { "listen", 'l', 0, G_OPTION_ARG_STRING, &opts->addr, "Address to listen on (see http://api.zeromq.org/3-2:zmq-tcp)", NULL },
-        { "version",  0, 0, G_OPTION_ARG_NONE, &show_version, "Show version information", NULL },
-        { NULL }
-    };
-
-    context = g_option_context_new (NULL);
-    g_option_context_add_main_entries (context, entries, NULL);
-
-    if (!g_option_context_parse (context, argc, argv, &error)) {
-        g_print ("Option parsing failed: %s\n", error->message);
-        g_free (opts);
-        return NULL;
-    }
-
-    if (show_version) {
-        g_print ("%s version " UFO_VERSION "\n", argv[0][0]);
-        exit (EXIT_SUCCESS);
-    }
-
-    if (opts->addr == NULL)
-        opts->addr = g_strdup ("tcp://*:5555");
-
-    g_option_context_free (context);
-
-    return opts;
-}
-
-static void
-opts_free (Options *opts)
-{
-    g_free (opts->addr);
-    g_free (opts);
-}
-
-static void
-terminate (int signum)
-{
-    if (signum == SIGTERM)
-        g_print ("Received SIGTERM, exiting...\n");
-
-    if (signum == SIGINT)
-        g_print ("Received SIGINT, exiting...\n");
-
-    done = TRUE;
-}
-
-int
-main (int argc, char * argv[])
-{
-    Options *opts;
-    GError *error = NULL;
-
-#if !(GLIB_CHECK_VERSION (2, 36, 0))
-    g_type_init ();
-#endif
-
-#if !(GLIB_CHECK_VERSION (2, 32, 0))
-    g_thread_init (NULL);
-#endif
-
-    if ((opts = opts_parse (&argc, &argv)) == NULL)
-        return 1;
-
-    /* Now, install SIGTERM/SIGINT handler */
-    (void) signal (SIGTERM, terminate);
-    (void) signal (SIGINT, terminate);
-
-    global_daemon = ufo_daemon_new (opts->addr, &error);
-
-    if (error != NULL)
-        goto error;
-
-    ufo_daemon_start (global_daemon, &error);
-
-    if (error != NULL)
-        goto error;
-
-    g_print ("ufod %s - waiting for requests on %s ...\n", UFO_VERSION, opts->addr);
-
-    while (!done)
-        g_usleep (G_USEC_PER_SEC);
-
-    ufo_daemon_stop (global_daemon, &error);
-
-error:
-    if (error != NULL)
-        g_printerr ("Error: %s\n", error->message);
-
-    g_object_unref (global_daemon);
-    opts_free (opts);
-
-    return error != NULL ? 1 : 0;
-}

bin/ufod.service.in
@@ -1,5 +1,0 @@
-[Unit]
-Description=Remote node daemon for UFO
-
-[Service]
-ExecStart=@CMAKE_INSTALL_FULL_BINDIR@/ufod --listen tcp://127.0.0.1:5000

config.h.in
@@ -1,7 +1,5 @@
 #cmakedefine WITH_PYTHON    1
 #cmakedefine HAVE_VIENNACL  1
-#cmakedefine WITH_ZMQ       1
-#cmakedefine WITH_MPI       1
 #define UFO_PLUGIN_DIR  "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_PLUGINDIR}"
 #define UFO_KERNEL_DIR  "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_KERNELDIR}"
 #define UFO_VERSION     "${UFO_VERSION}"

config.h.meson.in
@@ -1,6 +1,4 @@
 #mesondefine WITH_PYTHON
-#mesondefine WITH_ZMQ
-#mesondefine WITH_MPI
 #mesondefine HAVE_VIENNACL
 #mesondefine UFO_PLUGIN_DIR
 #mesondefine UFO_KERNEL_DIR

commit 3d9277fb9f67a9e502d374202ad8dab5807376cb

Fix #146: multi GPU expansion broke
This was introduced in ec70e0631 as part of the remote node removal.

ufo/ufo-task-graph.c
@@ -436,6 +436,7 @@ map_proc_node (UfoGraph *graph,
     UfoNode *proc_node;
     GList *successors;
     GList *it;
+    guint n_gpus;
 
     proc_node = UFO_NODE (g_list_nth_data (gpu_nodes, proc_index));
 
@@ -448,10 +449,13 @@ map_proc_node (UfoGraph *graph,
         ufo_task_node_set_proc_node (UFO_TASK_NODE (node), proc_node);
     }
 
+    n_gpus = g_list_length (gpu_nodes);
     successors = ufo_graph_get_successors (graph, node);
 
-    g_list_for (successors, it)
+    g_list_for (successors, it) {
         map_proc_node (graph, UFO_NODE (it->data), proc_index, gpu_nodes);
+        proc_index = n_gpus > 0 ? (proc_index + 1) % n_gpus : 0;
+    }
 
     g_list_free (successors);
 }