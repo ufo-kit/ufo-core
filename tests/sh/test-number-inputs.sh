194483e69175fa02d83aab79090e8d381d54e183
Marcus Zuber <marcus.zuber@kit.edu>
2020-05-14 16:09:17 +0200

    Enlarge dummy data

--- tests/sh/test-number-inputs.sh
+++ tests/sh/test-number-inputs.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-ufo-launch [dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100 , \
-	    dummy-data number=100] ! opencl filename=test-number-inputs.cl kernel=test_number_inputs dimensions=2 ! write filename=test.tif
+ufo-launch [dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256 , \
+	    dummy-data number=100 width=256 height=256] ! opencl filename=test-number-inputs.cl kernel=test_number_inputs dimensions=2 ! write filename=test.tif
 exit 0
