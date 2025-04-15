ac04a4c896c1c6bda8d2b8a4561ac9fafd22928b
Tomas Farago <sensej007@email.cz>
2025-04-15 17:37:20 +0200

    docs: update to Ubuntu 24.04.2

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -46,14 +46,7 @@ primary_domain = 'c'
 add_function_parentheses = True
 pygments_style = 'sphinx'
 
-try:
-    import sphinx_rtd_theme
-
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-except ImportError:
-    pass
-
+html_theme = 'sphinx_rtd_theme'
 html_logo = "_static/ufo-logo.png"
 html_static_path = ['_static']
 html_use_smartypants = True
